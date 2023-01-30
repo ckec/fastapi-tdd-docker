d39e5ed3c95bbb6b24d28c222f8f3c3c60bca371
ckechad <ckechad@icloud.com>
2023-01-30 16:11:45 +0100

    core(init pytest)

---
+++ project/tests/conftest.py
@@ -0,0 +1,22 @@
+import os
+
+import pytest
+
+from starlette.testclient import TestClient
+
+from app import main
+from app.config import get_settings, Settings
+
+
+def get_settings_override():
+    return Settings(testing=1, database_url=os.environ.get("DATABASE_URL"))
+
+
+@pytest.fixture
+def test_app():
+    main.app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(main.app) as test_client:
+        # testing
+        yield test_client
+
+    # tear down
